72614979a10022acdcda9414bd58f002ceec96c4
Damian Arya <dahakcyrus@yahoo.com>
2019-10-09 17:42:06 -0600

    adding snap challenge files

---
+++ snap-10-9/script.js
@@ -0,0 +1,27 @@
+// first half of chanenge
+
+let mult = 2*4;
+
+let string = "String 1" + "String 2";
+
+console.log(string)
+
+//second half of challenge
+
+let x=5
+
+function addNumber (n, m) {
+	console.log(x);
+
+	let b;
+	if(1 === 1) {
+		b = 8;
+	}
+	console.log(b);
+	let newVar = n + m;
+}
+
+
+function  concatStrings(firstName, lastName) {
+	return firstname + lastName
+}
