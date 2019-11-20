7c8884685c02959e6c542a2b33021250968d12f7
Damian Arya <dahakcyrus@yahoo.com>
2019-11-20 08:16:41 -0700

    got first part down

---
+++ snap-11-20/snap-11-20.js
@@ -0,0 +1,49 @@
+/*
+part one, return an array wof #s with count of duplicates.
+ */
+
+
+function compressArray(original) {
+
+	var compressed = [];
+	// make a copy of the input array
+	var copy = original.slice(0);
+
+	// first loop goes over every element
+	for (var i = 0; i < original.length; i++) {
+
+		var myCount = 0;
+		// loop over every element in the copy and see if it's the same
+		for (var w = 0; w < copy.length; w++) {
+			if (original[i] == copy[w]) {
+				// increase amount of times duplicate is found
+				myCount++;
+				// sets item to undefined
+				delete copy[w];
+			}
+		}
+
+		if (myCount > 0) {
+			var a = new Object();
+			a.value = original[i];
+			a.count = myCount;
+			compressed.push(a);
+		}
+	}
+
+	return compressed;
+};
+
+// It should go something like this:
+var testArray = new Array("1", "1", "2", "7", "5", "2", "9", "2");
+var newArray = compressArray(testArray);
+
+/*results could look like this
+console: [
+	Object { value="1", count=2},
+	Object { value="2", count=3},
+	Object { value="7", count=1},
+	Object { value="5", count=1},
+	Object { value="9", count=1}
+]
+*/
