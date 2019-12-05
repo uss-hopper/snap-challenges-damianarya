bb7785901a21723e6800dfe3f0bd7e6d79fd535a
Damian Arya <dahakcyrus@yahoo.com>
2019-12-05 09:05:33 -0700

    swing two

--- snap-12-5/snap-12-5.js
+++ snap-12-5/snap-12-5.js
@@ -4,4 +4,21 @@ axios({
 	data: {
 		target: 'all the goodest bois',
 	}
-});
+);
+//try 2
+
+const options = {
+	method: 'post',
+	url: 'https://dog.ceo/dog-api/breeds-list',
+	data: {
+		target: 'doggos!',
+	},
+	transformResponse: [(breeds) => {
+		// transform the response
+
+		return breeds;
+	}]
+};
+
+// send the request
+axios(options);
